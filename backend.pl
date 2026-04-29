46c4befafb1de4235dbfbf044739fd15a52e4361
peshi24 <epunuganti@gmail.com>
2026-04-29 14:31:23 -0500

    initial setup: included devlog file and prolog file

---
+++ backend.pl
@@ -0,0 +1,7 @@
+
+%Name: Eshitasri Punuganti
+%Date: 04/29/2026
+%Purpose: create a prolog backend that will allow a user to prepare a work schedule based on the facts given
+
+%code for project 2
+
