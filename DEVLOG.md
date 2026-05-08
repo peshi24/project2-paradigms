be348a40adb612b59a931f308b1c6a26c79d825b
peshi24 <epunuganti@gmail.com>
2026-05-08 15:02:21 -0500

    assign_workstation initial code

--- DEVLOG.md
+++ DEVLOG.md
@@ -30,3 +30,7 @@ to unify the plan predicate, i comnbined made plan/3 an input of plan/1. then, i
 
 I think for this session, the hardest part was definitely trying to figure out what predicates i needed to create and how i was going to make sure I was meeting all the requirements. I had to search up a few things because I forgot about the uppercase and lowercase syntax in prolog 😭 but I think once I started working on it and going forward it made a lot more sense to me, it was relatively easy. The syntax is definitely more straight forward once you've written a few lines.
 
+# May 8th, 2:16 pm
+
+second code sesh! for this session my goal is to finish the assign shift
+function and then start thinking about how to assign workstations to the employees based on all the facts provided in the input prolog file. i think i will do this by using a recursive function for the workstations. i would probably go through the list and then update the list by eliminating the worker that has been assigned. but i would also have to keep track of the assigned employees so that way i can delete them from the list after every workstation assignment. i'm also going to need a function to input the min and the max (so a unification function) and then i can apply that. i think this might require me to also create a subtract function to subtract the employees because i'm not sure prolog has a subtract function especially for lists. it has a subtract/3 function but i have to see how it works. ok lets go code, because i think this is a good thought process. 
--- backend.pl
+++ backend.pl
@@ -11,6 +11,8 @@ workstation_idle(_,_) :- FAIL.
 avoid_workstation(_,_) :- FAIL.
 avoid_shift(_,_) :- FAIL.
 
+% ------------------------------------------------------------------------------------------------------------------------
+
 % main predicate PLAN
 plan(plan(Morning, Evening, Night)) :-
 
@@ -29,4 +31,88 @@ plan(plan(Morning, Evening, Night)) :-
     %every employee must be assigned once (so there should be an empty list leftover)
     ReaminingNight = [].
 
+% ------------------------------------------------------------------------------------------------------------------------
 
+% assign_shift function -> creates schedule for each shift
+% parameters: shift, employees that have been assigned, employees that haven't been assigned, and the shift list
+
+assign_shift(Shift, AvailableEmployees, ShiftList, RemainingEmployees) :-
+
+    % get all the workstations from the facts doc that ARE NOT IDLE
+    % \+ is a negation as failure operator
+
+    findall(W, 
+        (
+            workstation(W, _, _),
+            \+ workstation_idle(W, Shift)
+        ),
+        ActiveWorkstations),
+
+    % build the schedule for the workstations
+
+    assign_workstations(ActiveWorkstations, Shift, AvailableEmployees, ShiftList, RemainingEmployees).
+
+% ------------------------------------------------------------------------------------------------------------------------
+
+% base case
+% no workstations left to assign (empty list)
+
+assign_workstations([], _, Employees, [], Employees).
+
+
+% recursive case
+% pull each element [ element | list ]
+
+assign_workstations(
+            [W | RestWorkstations],
+            Shift,
+            AvailableEmployees,
+            [workstation(W, AssignedEmployees) | RestPlan],
+            RemainingEmployees
+        ) :-
+
+    % get the workstation limits from the facts doc (same format)
+    workstation(W, Min, Max),
+
+    % choose the valid employees, so available employees
+    choose_workers(W, Shift, AvailableEmployees, Min, Max, AssignedEmployees),
+
+    % remove the assigned employees through the subtract function
+    subtract(AvailableEmployees, AssignedEmployees, NewAvailableEmployees),
+
+    % continue the recursion with remaining elements
+    assign_workstations(RestWorkstations, Shift, NewAvailableEmployees, RestPlan, RemainingEmployees).
+
+% ------------------------------------------------------------------------------------------------------------------------
+
+% choose workers function to select a valid employee group for the workstation
+choose_workers(W, Shift, AvailableEmployees, Min, Max, AssignedEmployees) :-
+
+    % pick the subset of the employees that need to be assigned (subset function)
+    subset(AssignedEmployees, AvailableEmployees),
+
+    % check the size constraints to make sure its within the bounds of min and max
+    length(AssignedEmployees, Count),
+    
+    Count >= Min,
+    Count =< Max,
+
+    % validate all the employees to make sure it still works with all the constraints (the avoidances)
+    validate(AssignedEmployees, W, Shift).
+
+% ------------------------------------------------------------------------------------------------------------------------
+
+% validate function to make sure the employee assignment fits the workstation and shift requirements
+
+validate([], _, _).
+
+validate([E | Rest], W, Shift) :-
+
+    % check if the employee NEEDS to avoid the workstation
+    \+ avoid_workstation(E, W),
+
+    % check if the employee NEEDS to avoid the shift
+    \+ avoid_shift(E, Shift),
+
+    % loop back to check the rest
+    validate(Rest, W, Shift).
